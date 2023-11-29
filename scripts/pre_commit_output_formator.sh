d09986a0fdd5004fe7c6a410437413a48c8792bc
Gabriel Suquet <91719818+gsuquet@users.noreply.github.com>
2023-11-29 19:34:11 +0100

    :lipstick: feat: Modify workflows to output nicer markdown summaries (#6)
    # Description
    
    Modify precommit and string validator workflows to have better and nicer
    formatted outputs. Added checks and visual indicators.
    
    ## Type of change
    
    :sparkles: New feature (non-breaking change which adds functionality)
    :memo: Documentation update
    
    # How Has This Been Tested?
    
    - Ran the scripts to check the markdown output locally
    - Used this PR to verify the workflows beahve according to expectations
    
    # Checklist:
    
    - [X] My code follows the style guidelines of this project
    - [X] I have performed a self-review of my own code
    - [X] I have made corresponding changes to the documentation
    - [X] My changes generate no new warnings
    - [X] I have added tests that prove my fix is effective or that my
    feature works
    - [X] New and existing unit tests pass locally with my changes
    - [X] Any dependent changes have been merged and published in downstream
    modules

--- scripts/pre_commit_output_formator.sh
+++ scripts/pre_commit_output_formator.sh
@@ -1,21 +1,29 @@
 output=$(cat ./test/pre_commit_output.txt)
-output=$(echo "$output" | grep -v '\[INFO\]')
+
+if echo "$output" | grep -q 'Failed'; then
+    echo "## :x: Pre-commit checks failed :x:" > ./test/pre_commit_output.md
+    echo "You can reproduce this behavior locally by running :" >> ./test/pre_commit_output.md
+    echo "\`\`\`bash" >> ./test/pre_commit_output.md
+    echo "pre-commit run --all-files --show-diff-on-failure" >> ./test/pre_commit_output.md
+    echo "\`\`\`" >> ./test/pre_commit_output.md
+    echo "Please fix the errors and commit again." >> ./test/pre_commit_output.md
+    echo "If you think this is a false positive, please open an issue on the [GitHub repository](https://github.com/gsuquet/workflows/issues/new?assignees=&labels=bug%2Cto+sort&projects=&template=bug_report.yml)." >> ./test/pre_commit_output.md
+    echo "" >> ./test/pre_commit_output.md
+else
+    echo "## :white_check_mark: Pre-commit checks passed :white_check_mark:\n" > ./test/pre_commit_output.md
+fi
+
+# Exclude lines that don't end with Passed, Skipped or Failed
+output=$(echo "$output" | grep -E 'Passed|Skipped|Failed')
+
 output=$(echo "$output" | sed 's/Passed/:white_check_mark:/g')
 output=$(echo "$output" | sed 's/Skipped/:ballot_box_with_check:/g')
 output=$(echo "$output" | sed 's/Failed/:x:/g')
 
-# Exclude empty lines
-output=$(echo "$output" | grep -v '^$')
-
-# Exclude lines that start with '-'
-output=$(echo "$output" | grep -v '^-')
-
-# Exclude lines that start with 'Fixing'
-output=$(echo "$output" | grep -v '^Fixing')
-
 # Use awk to format the output into a markdown table, removing the dots
 output=$(echo "$output" | awk -F':' '{gsub(/\.+/, "", $1); print "| " $1 " | :" $2 ":|"}')
 
-echo "| Check | Result |" > ./test/pre_commit_output.md
+echo "### Checks summary" >> ./test/pre_commit_output.md
+echo "| Check | Result |" >> ./test/pre_commit_output.md
 echo "| --- | --- |" >> ./test/pre_commit_output.md
 echo "$output" >> ./test/pre_commit_output.md
--- test/pre_commit_output.md
+++ test/pre_commit_output.md
@@ -1,3 +1,12 @@
+## :x: Pre-commit checks failed :x:
+You can reproduce this behavior locally by running :
+```bash
+pre-commit run --all-files --show-diff-on-failure
+```
+Please fix the errors and commit again.
+If you think this is a false positive, please open an issue on the [GitHub repository](https://github.com/gsuquet/workflows/issues/new?assignees=&labels=bug%2Cto+sort&projects=&template=bug_report.yml).
+
+### Checks summary
 | Check | Result |
 | --- | --- |
 | check for merge conflicts | :white_check_mark:|
@@ -12,5 +21,4 @@
 | Pretty format YAML | :white_check_mark:|
 | check for added large files | :white_check_mark:|
 | check for added large files with git lfs | :x:|
-| Fixing test/pre_commit_outputtxt | ::|
 | check for added large files with git lfs | :white_check_mark:|
